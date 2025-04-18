ec1ad3d5320e1c76c49eca3efa8cdf4791e36d0a
alminkevich <alminkevich@gmail.com>
2025-04-18 08:20:36 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, ContextTypes, filters
+
+TOKEN = "8180115203:AAGT97dkcj5EYbTd5J4T_vy-Uq3N7yDNvCM"
+WEB_APP_URL = "https://7k-gaming.onrender.com"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [KeyboardButton(text="Открыть мини-апку", web_app=WebAppInfo(url=WEB_APP_URL))]
+    ]
+    markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await update.message.reply_text("Нажми кнопку ниже:", reply_markup=markup)
+
+async def handle_web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    data = update.message.web_app_data.data
+    await update.message.reply_text(f"Ты отправил: {data}")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, handle_web_app_data))
+    app.run_polling()
